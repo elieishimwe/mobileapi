5d621743f8dd1e445b95d5e4406e61e573d578a4
Elie Ishimwe <elieish@gmail.com>
2015-07-13 15:49:55 +0200

    updates

--- www/js/app.js
+++ www/js/app.js
@@ -3,27 +3,12 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
 
-
-var app       = angular.module('starter', ['ionic', 'starter.services', 'starter.controllers']);
-var apiRoot   = 'siyaleader.co.za:8080/ecin2edin/console/app_backend/port_backend/public';
-//var apiRoot = 'http://localhost:8000/';
-var xhReq   = new XMLHttpRequest();
-xhReq.open("GET", "//" + apiRoot + "/api/csrf", false);
-xhReq.send(null);
-
-
-app.constant("CSRF_TOKEN", xhReq.responseText);
-
-app.run(function($ionicPlatform, $http,CSRF_TOKEN) {
-    //$http.defaults.headers.common['X-CSRF-TOKEN'] = CSRF_TOKEN;
+.run(function($ionicPlatform, $http) {
     $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
-
-
-
-
         if (window.cordova && window.cordova.plugins.Keyboard) {
             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
         }
@@ -35,12 +20,12 @@ app.run(function($ionicPlatform, $http,CSRF_TOKEN) {
 
         var apiKey = localStorage.getItem("key");
         var user_email = localStorage.getItem("user_email");
-        $http.defaults.headers.common.api_key = apiKey.toString();
+        $http.defaults.headers.common.Authorization = apiKey.toString();
     }
 
-});
+})
 
-app.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
 
     // $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|blob|cdvfile|content):|data:image\//);
 
@@ -54,15 +39,6 @@ app.config(function($stateProvider, $urlRouterProvider) {
             templateUrl: "templates/menu.html",
             controller: 'AppCtrl'
         })
-
-      /*  .state('app', {
-            url: "/app",
-            abstract: true,
-            templateUrl: "templates/login.html",
-            controller: 'LoginCtrl'
-        })*/
-
-
         .state('app.home', {
             url: "/home",
             views: {
